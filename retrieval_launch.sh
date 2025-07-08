40eae948b73ee00399007dc535302594340e07d4
Yue Lu <0625072@zju.edu.cn>
2025-07-08 15:52:50 +0800

    Update retrieval_launch.sh
    根据沙盒环境更新

--- retrieval_launch.sh
+++ retrieval_launch.sh
@@ -1,13 +1,9 @@
-
-file_path=/the/path/you/save/corpus
-index_file=$file_path/e5_Flat.index
-corpus_file=$file_path/wiki-18.jsonl
-retriever_name=e5
-retriever_path=intfloat/e5-base-v2
-
-python search_r1/search/retrieval_server.py --index_path $index_file \
-                                            --corpus_path $corpus_file \
-                                            --topk 3 \
-                                            --retriever_name $retriever_name \
-                                            --retriever_model $retriever_path \
-                                            --faiss_gpu
+export save_path=/home/jovyan/work_vol90/RL+RAG/Search-R1-main/indexing_corpus
+export index_file=$save_path/e5_Flat.index
+export corpus_file=$save_path/wiki-18.jsonl
+export retriever_name=e5
+#export retriever_path=intfloat/e5-base-v2
+export retriever_path="/home/jovyan/work_vol90/RL+RAG/Search-R1-main/models/intfloat_e5-base-v2"
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+python search_r1/search/retrieval_server.py --index_path $index_file --corpus_path $corpus_file --topk 3 --retriever_name $retriever_name --retriever_model $retriever_path --faiss_gpu
