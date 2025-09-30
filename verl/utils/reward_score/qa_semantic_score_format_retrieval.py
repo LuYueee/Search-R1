77bf03345b0a8b5f77f27926bc6c8b84bad74615
Yue Lu <0625072@zju.edu.cn>
2025-09-30 09:57:31 +0800

    Update qa_semantic_score_format_retrieval.py

--- verl/utils/reward_score/qa_semantic_score_format_retrieval.py
+++ verl/utils/reward_score/qa_semantic_score_format_retrieval.py
@@ -277,7 +277,8 @@ def compute_score_em(solution_str, ground_truth, model_path, structure_format_sc
     """
     is_valid_format, _ = is_valid_sequence(solution_str)
 
-    answer = extract_solution(solution_str=solution_str)
+    response_str = solution_str[solution_str.find('<|im_start|>assistant') + 21:] if '<|im_start|>assistant' in solution_str else solution_str
+    answer = extract_solution(response_str)
 
     ###################
     do_print = random.randint(1, 64) == 1
@@ -320,7 +321,6 @@ def compute_score_em(solution_str, ground_truth, model_path, structure_format_sc
         final_em_format_score = max(0.0, min(1.0, final_em_format_score))
     
     # Rewards for redundant retrieval
-    response_str = solution_str[solution_str.find('<|im_start|>assistant') + 21:] if '<|im_start|>assistant' in solution_str else solution_str
     n_search = count_search_tags(response_str)
     n_repeat = count_repeat_information(response_str)  
 
